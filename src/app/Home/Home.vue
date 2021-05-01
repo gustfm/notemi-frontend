433788b3785407c4615afcd7dc1bd041bdf3aa7a
Gustavo <gustavo@solucx.com.br>
2021-05-01 10:00:47 -0300

    Init repository

---
+++ src/app/Home/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app">
+    <Sidebar />
+    <div>a</div>
+    <div>b</div>
+    <!-- <NotesListContainer /> -->
+    <!-- <NoteContainer /> -->
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+import Sidebar from "@/components/Sidebar.vue";
+
+@Component({
+  components: {
+    Sidebar,
+  },
+})
+export default class Home extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.app {
+  display: grid;
+  grid-template-columns: 240px 380px 1fr;
+}
+</style>
---
+++ src/app/Home/components/NotesListContainer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="note-list__container">
+    <div>Private</div>
+    <div class="sidebar__menu"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Sidebar extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.sidebar {
+  background-color: #1f1f1f;
+//   width: 280px;
+  height: 100vh;
+}
+</style>
